7090fdd1741ececb6dd37a53db16ca8fc0bb5537
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-13 08:22:05 +0530

    Add files via upload

---
+++ 1.Introduction/Program to Multiply two Numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{
+    int n1,n2,mul;
+
+
+    //Get the Values from User
+    std::cout<<"Enter the FirstNumber: ";
+    std::cin>>n1;
+    std::cout<<"Enter the Second Number: ";
+    std::cin>>n2;
+
+    mul = n1*n2;
+
+    //Print the Multiplied Value
+    std::cout<<"Multiplied Value: "<<mul;
+
+    return 0;
+}
