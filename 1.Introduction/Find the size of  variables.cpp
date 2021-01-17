cfc440af2eab90ef66d5c42c54a69c1627c4a35b
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-17 14:17:49 +0530

    Add files via upload

---
+++ 1.Introduction/Find the size of  variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    std::cout<<"Size of integer: "<<sizeof(int)<<"bytes"<<"\n";
+    std::cout<<"Size of float: "<<sizeof(float)<<"bytes"<<"\n";
+    std::cout<<"Size of character: "<<sizeof(char)<<"bytes"<<"\n";
+    std::cout<<"Size of Double Integer: "<<sizeof(double)<<"bytes"<<"\n";
+
+    return 0;;
+}
