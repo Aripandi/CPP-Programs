d9a60dfba11f1927cda6f6900f5bc534047d9663
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-17 14:19:28 +0530

    Add files via upload

---
+++ 1.Introduction/Finding the ASCII Value for Characters.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    cout<<"Enter the Character: ";
+    cin>>x;
+    cout<<"ASCII Value of "<<x<<" is: "<<int(x);
+
+    return 0;
+}
