ef8f26d2c1d9ae8f7ab256b44da936a67443a803
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-22 07:50:56 +0530

    Add files via upload

---
+++ 2.Decision Making and Loops/Reverse the digits.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num,rev = 0;
+    cout<<"Enter the Digits: ";
+    cin>>num;
+
+    while(num>0)
+    {
+        rev =(rev*10)+num%10;
+        num /= 10;
+    }
+
+    cout<<"Reversed Digits: "<<rev;
+    return 0;
+}
