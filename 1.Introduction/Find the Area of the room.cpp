13b08bf52acc1cd3a10a98923565dc9aec43be9e
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-28 11:59:12 +0530

    Create Find the Area of the room.cpp

---
+++ 1.Introduction/Find the Area of the room.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int room_width;
+    int room_height;
+    
+    cout<<"Enter width: ";
+    cin>>room_width;
+    
+    cout<<"Enter height: ";
+    cin>>room_height;
+    
+    cout<<"The Area of the room is "<<room_width*room_height<<" Sqaure feet";
+    
+    return 0;
+}
