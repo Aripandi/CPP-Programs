c4e453f3d72b6a7060f481d1f0d01ab9d9a25fc9
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-04-03 14:42:26 +0530

    Update Example for nested loop.cpp

--- Random Programs/Example for nested loop.cpp
+++ Random Programs/Example for nested loop.cpp
@@ -2,3 +2,20 @@
 #include<vector>
 
 using namespace std;
+
+int main()
+{
+    vector<int> numbers{};
+    char choice{};
+
+    do{
+        //Display the menu
+        cout<<"\nP-Print the Numbers in List"<<endl;
+        cout<<"A--Add the numbers to list"<<endl;
+        cout<<"m-find the mean of the list"<<endl;
+        cout<<"S-Display the smallest number"<<endl;
+        cout<<"L-Display the Largest number"<<endl;
+        cout<<"Q-Quit the Process"<<endl;
+        cout<<"\nEnter the choice: ";
+        cin>>choice;
+
