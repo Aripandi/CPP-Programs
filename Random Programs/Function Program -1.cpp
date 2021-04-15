feb8d3a36a79c31728a8f547d189ee710910d1db
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-04-15 13:09:05 +0530

    Create Function Program -1.cpp

---
+++ Random Programs/Function Program -1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<cctype> //used for toupper
+
+using namespace std;
+
+//functions for dispplay the list and get velues to be selected
+void display_menu();
+char get_selection();
