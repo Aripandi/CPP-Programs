bf85080800a642cc03bce525b9966e2e95f24741
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-04-15 13:09:42 +0530

    Update Function Program -1.cpp

--- Random Programs/Function Program -1.cpp
+++ Random Programs/Function Program -1.cpp
@@ -7,3 +7,13 @@ using namespace std;
 //functions for dispplay the list and get velues to be selected
 void display_menu();
 char get_selection();
+
+//funtions to handle after got the value from the user
+void handle_display(const vector<int> &v);
+void handle_add(vector<int> &v);
+void handle_mean(const vector<int> &v);
+void handle_smallest(const vector<int> &v);
+void handle_largest(const vector<int> &v);
+void handle_find(const vector<int> &v);
+void handle_quit();
+void handle_unknown();
