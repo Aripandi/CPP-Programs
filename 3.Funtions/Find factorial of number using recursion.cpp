8e84294dc685cfde3e0ad60b77d3aba6f48e764b
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-05-11 07:38:19 +0530

    Update Find factorial of number using recursion.cpp

--- 3.Funtions/Find factorial of number using recursion.cpp
+++ 3.Funtions/Find factorial of number using recursion.cpp
@@ -1,1 +1,24 @@
-#incliude
+#include<iostream>
+using namespace std;
+
+int fact(int n);
+int main()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    cout<<"Factorial of the number: "<<fact(n);
+
+    return 0;
+}
+
+int fact(int n)
+{
+    if(n==0)
+        return 1;
+
+    else
+        return n*fact(n-1);
+}
+
